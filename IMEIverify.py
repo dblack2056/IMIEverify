9e485ccb7f1ba892f9f8cfa971a854cfb7af5156
dblack2056 <71357479+dblack2056@users.noreply.github.com>
2020-09-19 21:10:02 -0600

    Update IMEIverify.py

--- IMEIverify.py
+++ IMEIverify.py
@@ -1,3 +1,14 @@
+#IMEIs are 14 digits + 1 check digit
+#The check digit is calculated using the Luhn algorithm
+#For the IMEI 12345678901234 the check digit would be 7 calculated using the following:
+#First double every second digit making the following 1,4,3,8,5,12,7,16,9,0,1,4,3,8
+#Break everything down into single digits (so 12 would become 1 and 2) and add it all together 1+4+3+8+5+1+2+7+1+6+9+0+1+4+3+8 = 63
+#The check digit is the amount that needs to be added to make the number divisible by 10 in the above case it can be calculated by the following: 10-(63%10)
+#For a better description of how to do this visit https://en.wikipedia.org/wiki/International_Mobile_Equipment_Identity#Check_digit_computation
+#Rather than breaking 2-digit numbers down into 2 separate digits I am just subtracting 9 since these numbers will always be 18 or less
+
+
+
 import os
 
 def validate_checksum(IMEI):
